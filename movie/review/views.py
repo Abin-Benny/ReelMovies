f7d376ef1b23af224956208e9606c8870a5611fa
Abin Benny <abin95benny@gmail.com>
2021-07-01 15:24:39 +0530

    sweet alert is added to display error message if searched movie is not found

--- movie/review/views.py
+++ movie/review/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render,redirect
 from review.models import details
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     if request.method=='POST':
         mname = request.POST.get('searchname')
         movies = details.objects.filter( movie_name=mname)
+        if movies.count()==0:
+            movies = details.objects.all()
+            messages.info(request, "Requested movie is not available.")
+            return render(request, "index.html", {'movies':movies})
     else:
         movies = details.objects.all()
     return render(request,"index.html",{'movies':movies})
